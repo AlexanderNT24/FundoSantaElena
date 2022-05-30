bb919e74d22741d840516f460bf94d3dfb684bbf
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2022-05-30 00:45:49 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FundoSantaElena.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
