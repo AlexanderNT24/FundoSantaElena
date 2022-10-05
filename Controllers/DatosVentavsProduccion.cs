787ae30e81eea5046761b00bcc66e20d5c04b161
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2022-10-05 07:34:55 -0500

    Agregue chatbot, vista y controlador

---
+++ Controllers/DatosVentavsProduccion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FundoSantaElena.Controllers
+{
+    public class DatosVentavsProduccion : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
