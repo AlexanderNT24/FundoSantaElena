4374ffb566507c0d6f4640dbb0aaa91fc4cebd04
Alexander Nureña <89727568+AlexanderNT24@users.noreply.github.com>
2022-10-26 04:01:53 -0500

    add Reporte entre fechas

--- FundoSantaElena/Controllers/DatosVentavsProduccion.cs
+++ FundoSantaElena/Controllers/DatosVentavsProduccion.cs
@@ -40,52 +40,117 @@ namespace FundoSantaElena.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Index(ProduccionAnimal pAnimales)
         {
-            
-            IEnumerable<ProduccionAnimal> produccionAnimal = _context.ProduccionAnimales.FromSqlRaw("SELECT 0 AS Id, Fecha,convert (nvarchar,SUM(convert (int,Cantidad)))as Cantidad,0 AS IdAnimal FROM[FundoSantaElena].[dbo].[ProduccionAnimales] WHERE Fecha ='" + pAnimales.Fecha + "' GROUP BY Fecha").ToList();
-            float totalProduccion = 0;
-            foreach (var item in produccionAnimal)
+            Debug.WriteLine("Entrada" + pAnimales.Fecha);
+            var fechas = pAnimales.Fecha.Split('/');
+
+            if (fechas.Length > 1)
             {
-                totalProduccion = float.Parse(item.Cantidad);
-            }
-            IEnumerable<Animal> animales = _context.Animales;
-            IEnumerable<VentaProduccion> ventaProduccion = _context.VentaProduccion.FromSqlRaw("SELECT [Id],[Fecha],[Destino],[Cantidad],[Precio] FROM[FundoSantaElena].[dbo].[VentaProduccion] WHERE Fecha = '" + pAnimales.Fecha + "'").ToList();
-            ViewBag.VentaProduccion = ventaProduccion;
-            IEnumerable<VentaProduccion> venta = _context.VentaProduccion.FromSqlRaw("SELECT 0 AS Id, Fecha,convert (nvarchar,SUM(convert (int,Cantidad)))as Cantidad, convert (nvarchar,SUM(convert (int,Precio)))as Precio ,'' AS Destino FROM[FundoSantaElena].[dbo].[VentaProduccion] WHERE Fecha = '" + pAnimales.Fecha + "' GROUP BY Fecha").ToList();
-            float ventaTotal = 0;
-            foreach (var item in venta)
-            {
-                ventaTotal = float.Parse(item.Precio);
-            }
-            float ventaCantidadTotal = 0;
-            foreach (var item in venta)
-            {
-                ventaCantidadTotal = float.Parse(item.Cantidad);
-            }
-            float totalProduccionAnimal=0;
-            if (pAnimales.IdAnimal == 0)
-            {
-                IEnumerable<ProduccionAnimal> produccionAnimales = _context.ProduccionAnimales.FromSqlRaw("SELECT [Id],[Fecha],[Cantidad],[IdAnimal]FROM[FundoSantaElena].[dbo].[ProduccionAnimales]WHERE Fecha = '" + pAnimales.Fecha + "'").ToList();
-                ViewBag.ProduccionAnimales = produccionAnimales;
+                var fechaActual = fechas[1];
+                var fechaRango = fechas[0];
+                IEnumerable<ProduccionAnimal> produccionAnimal = _context.ProduccionAnimales.FromSqlRaw("SELECT 0 AS Id, '' as Fecha,convert (nvarchar,SUM(convert (int,Cantidad)))as Cantidad,0 AS IdAnimal FROM[FundoSantaElena].[dbo].[ProduccionAnimales] WHERE [Fecha] BETWEEN '"+ fechaRango + "' AND '"+ fechaActual+"'").ToList();
+                float totalProduccion = 0;
+               
+                foreach (var item in produccionAnimal)
+                {
+                    totalProduccion = float.Parse(item.Cantidad);
+                }
+                IEnumerable<Animal> animales = _context.Animales;
+                IEnumerable<VentaProduccion> ventaProduccion = _context.VentaProduccion.FromSqlRaw("SELECT [Id],[Fecha],[Destino],[Cantidad],[Precio] FROM[FundoSantaElena].[dbo].[VentaProduccion] WHERE [Fecha] BETWEEN '"+ fechaRango + "' AND '"+ fechaActual+"'").ToList();
+                ViewBag.VentaProduccion = ventaProduccion;
+                var sql = "SELECT 0 AS Id, '' AS Fecha,convert (nvarchar,SUM(convert (int,Cantidad)))as Cantidad, convert (nvarchar,SUM(convert (int,Precio)*convert (int,Cantidad)))as Precio ,'' AS Destino FROM[FundoSantaElena].[dbo].[VentaProduccion] WHERE [Fecha] BETWEEN '" + fechaRango + "' AND '" + fechaActual + "'";
+                Debug.WriteLine(" SQL -------------------------"+sql);
+                IEnumerable<VentaProduccion> venta = _context.VentaProduccion.FromSqlRaw(sql).ToList();
+
+                float ventaTotal = 0;
+                foreach (var item in venta)
+                {
+                    ventaTotal = float.Parse(item.Precio);
+                }
+                float ventaCantidadTotal = 0;
+                foreach (var item in venta)
+                {
+                    ventaCantidadTotal = float.Parse(item.Cantidad);
+                }
+                float totalProduccionAnimal = 0;
+                if (pAnimales.IdAnimal == 0)
+                {
+                    IEnumerable<ProduccionAnimal> produccionAnimales = _context.ProduccionAnimales.FromSqlRaw("SELECT [Id],[Fecha],[Cantidad],[IdAnimal]FROM[FundoSantaElena].[dbo].[ProduccionAnimales]WHERE [Fecha] BETWEEN '" + fechaRango + "' AND '" + fechaActual + "'").ToList();
+                    ViewBag.ProduccionAnimales = produccionAnimales;
+
+                }
+                else
+                {
+                    IEnumerable<ProduccionAnimal> produccionAnimales = _context.ProduccionAnimales.FromSqlRaw("SELECT [Id],[Fecha],[Cantidad],[IdAnimal]FROM[FundoSantaElena].[dbo].[ProduccionAnimales] WHERE Fecha BETWEEN '" + fechaRango + "' AND '" + fechaActual + "' AND IdAnimal=" + pAnimales.IdAnimal).ToList();
+                    ViewBag.ProduccionAnimales = produccionAnimales;
+                    foreach (var item in produccionAnimales)
+                    {
+                        totalProduccionAnimal = float.Parse(item.Cantidad) + totalProduccionAnimal;
+                    }
+                }
+                ViewBag.TotalProduccionAnimal = totalProduccionAnimal;
+                ViewBag.Post = true;
+                ViewBag.Animales = animales;
+                
+                ViewBag.Perdida = totalProduccion - ventaCantidadTotal;
+                ViewBag.TotalVentaCantidad = ventaCantidadTotal;
+                ViewBag.TotalVenta = ventaTotal;
+                ViewBag.TotalProduccion = totalProduccion;
+                Debug.WriteLine("Fecha" + pAnimales.IdAnimal);
 
             }
-            else
+            
+            else 
             {
-                IEnumerable<ProduccionAnimal> produccionAnimales = _context.ProduccionAnimales.FromSqlRaw("SELECT [Id],[Fecha],[Cantidad],[IdAnimal]FROM[FundoSantaElena].[dbo].[ProduccionAnimales] WHERE Fecha = '" + pAnimales.Fecha + "' AND IdAnimal=" + pAnimales.IdAnimal).ToList();
-                ViewBag.ProduccionAnimales = produccionAnimales;
-                foreach (var item in produccionAnimales)
+                IEnumerable<ProduccionAnimal> produccionAnimal = _context.ProduccionAnimales.FromSqlRaw("SELECT 0 AS Id, Fecha,convert (nvarchar,SUM(convert (int,Cantidad)))as Cantidad,0 AS IdAnimal FROM[FundoSantaElena].[dbo].[ProduccionAnimales] WHERE Fecha ='" + pAnimales.Fecha + "' GROUP BY Fecha").ToList();
+                float totalProduccion = 0;
+                foreach (var item in produccionAnimal)
                 {
-                    totalProduccionAnimal = float.Parse(item.Cantidad)+ totalProduccionAnimal;
+                    totalProduccion = float.Parse(item.Cantidad);
                 }
+                IEnumerable<Animal> animales = _context.Animales;
+                IEnumerable<VentaProduccion> ventaProduccion = _context.VentaProduccion.FromSqlRaw("SELECT [Id],[Fecha],[Destino],[Cantidad],[Precio] FROM[FundoSantaElena].[dbo].[VentaProduccion] WHERE Fecha = '" + pAnimales.Fecha + "'").ToList();
+                ViewBag.VentaProduccion = ventaProduccion;
+                IEnumerable<VentaProduccion> venta = _context.VentaProduccion.FromSqlRaw("SELECT 0 AS Id, Fecha,convert (nvarchar,SUM(convert (int,Cantidad)))as Cantidad, convert (nvarchar,SUM(convert (int,Precio)*convert (int,Cantidad)))as Precio ,'' AS Destino FROM[FundoSantaElena].[dbo].[VentaProduccion] WHERE Fecha = '" + pAnimales.Fecha + "' GROUP BY Fecha").ToList();
+
+                float ventaTotal = 0;
+                foreach (var item in venta)
+                {
+                    ventaTotal = float.Parse(item.Precio);
+                }
+                float ventaCantidadTotal = 0;
+                foreach (var item in venta)
+                {
+                    ventaCantidadTotal = float.Parse(item.Cantidad);
+                }
+                float totalProduccionAnimal = 0;
+                if (pAnimales.IdAnimal == 0)
+                {
+                    IEnumerable<ProduccionAnimal> produccionAnimales = _context.ProduccionAnimales.FromSqlRaw("SELECT [Id],[Fecha],[Cantidad],[IdAnimal]FROM[FundoSantaElena].[dbo].[ProduccionAnimales]WHERE Fecha = '" + pAnimales.Fecha + "'").ToList();
+                    ViewBag.ProduccionAnimales = produccionAnimales;
+
+                }
+                else
+                {
+                    IEnumerable<ProduccionAnimal> produccionAnimales = _context.ProduccionAnimales.FromSqlRaw("SELECT [Id],[Fecha],[Cantidad],[IdAnimal]FROM[FundoSantaElena].[dbo].[ProduccionAnimales] WHERE Fecha = '" + pAnimales.Fecha + "' AND IdAnimal=" + pAnimales.IdAnimal).ToList();
+                    ViewBag.ProduccionAnimales = produccionAnimales;
+                    foreach (var item in produccionAnimales)
+                    {
+                        totalProduccionAnimal = float.Parse(item.Cantidad) + totalProduccionAnimal;
+                    }
+                }
+                ViewBag.TotalProduccionAnimal = totalProduccionAnimal;
+                ViewBag.Post = true;
+                ViewBag.Animales = animales;
+                ViewBag.Perdida = totalProduccion - ventaCantidadTotal;
+                ViewBag.TotalVentaCantidad = ventaCantidadTotal;
+                ViewBag.TotalVenta = ventaTotal;
+                ViewBag.TotalProduccion = totalProduccion;
+                Debug.WriteLine("Fecha" + pAnimales.IdAnimal);
+
             }
-            ViewBag.TotalProduccionAnimal = totalProduccionAnimal;
-            ViewBag.Post = true;
-            ViewBag.Animales = animales;
-            ViewBag.Perdida = totalProduccion - ventaCantidadTotal;
-            ViewBag.TotalVentaCantidad = ventaCantidadTotal;
-            ViewBag.TotalVenta = ventaTotal;
-            ViewBag.TotalProduccion = totalProduccion;
-            Debug.WriteLine("Fecha"+pAnimales.IdAnimal);
+          
             return View();
+
         }
     }
 }
